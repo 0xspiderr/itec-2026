f1d3c495356138ea93c43d4aa70c09c13df420cc
Anollex <r.mih99@gmail.com>
2026-03-27 23:27:19 +0200

    warrnig solved

--- _mainMenu/main_menu.gd
+++ _mainMenu/main_menu.gd
@@ -2,7 +2,7 @@ extends MarginContainer
 
 @onready var ip_addres: LineEdit = $menuContainer/IPAddres
 @onready var name_player: LineEdit = $menuContainer/Name
-@onready var connect: Button = $menuContainer/Connect
+@onready var connect_button: Button = $menuContainer/Connect
 @onready var menu: VBoxContainer = $menuContainer
 @onready var error: CenterContainer = $CenterContainer
 
@@ -12,15 +12,15 @@ extends MarginContainer
 func _ready() -> void:
 	ip_addres.text_changed.connect(_check_fields)
 	name_player.text_changed.connect(_check_fields)
-	connect.disabled = true
-	connect.pressed.connect(_on_button_pressed)
+	connect_button.disabled = true
+	connect_button.pressed.connect(_on_button_pressed)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _check_fields(new_text: String = "") -> void:
+func _check_fields(_new_text: String = "") -> void:
 	if ip_addres.text.is_empty() or name_player.text.is_empty():
-		connect.disabled = true
+		connect_button.disabled = true
 	else:
-		connect.disabled = false
+		connect_button.disabled = false
 
 func _on_button_pressed() -> void:
 	if ip_addres.text.is_valid_ip_address():
