678858bd20dc8b3a881414c3eb40011c312d2c5c
Harry <harypatrascu.1@gmail.com>
2026-03-27 22:39:55 +0200

    added: player, still figuring out some bugs

--- _level/level_manager.gd
+++ _level/level_manager.gd
@@ -3,15 +3,20 @@ class_name LevelManager extends Node2D
 
 const RAT = preload("uid://do03p4467dnyq")
 @onready var players: Node = $Players
+@onready var spawn_points: Node = $SpawnPoints
 
 
 func _ready() -> void:
-	_spawn_rat()
+	_spawn_rats()
 
 
-func _spawn_rat() -> void:
+func _spawn_rats() -> void:
 	if not multiplayer.is_server():
 		return
 	
-	var new_rat = RAT.instantiate() as RatController
-	players.add_child(new_rat)
+	var spawns = spawn_points.get_children()
+	for i in multiplayer.get_peers().size() + 1:
+		var new_rat = RAT.instantiate() as RatController
+		var spawn_point = spawns.pop_front()
+		new_rat.position = spawn_point.position
+		players.add_child(new_rat, true)
--- _rat/rat.gd
+++ _rat/rat.gd
@@ -4,6 +4,19 @@ class_name RatController extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@onready var camera_2d: Camera2D = $Camera2D
+@onready var input_component: InputComponent = $InputComponent
 
-func _physics_process(delta: float) -> void:
-	pass
+
+func _ready() -> void:
+	# set input authority to the local player
+	%InputComponent.set_multiplayer_authority(multiplayer.get_unique_id())
+	camera_2d.make_current()
+
+
+func _physics_process(_delta: float) -> void:
+	if not multiplayer.is_server():
+		return
+	velocity = input_component.direction * SPEED
+	
+	move_and_slide()
