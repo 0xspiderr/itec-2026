cc050db39311a18842d5bea9454179496531e527
Harry <harypatrascu.1@gmail.com>
2026-03-27 23:07:39 +0200

    added: rat animations and sync

--- _rat/rat.gd
+++ _rat/rat.gd
@@ -11,6 +11,7 @@ const SPEED = 300.0
 
 @onready var camera_2d: Camera2D = $Camera2D
 @onready var input_component: InputComponent = $InputComponent
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 
 func _ready() -> void:
@@ -22,6 +23,22 @@ func _physics_process(_delta: float) -> void:
 	if not multiplayer.is_server():
 		return
 	
-	velocity = input_component.direction * SPEED
-	
+	_move_player()
 	move_and_slide()
+
+
+func _move_player() -> void:
+	if input_component.direction == Vector2.ZERO:
+		animated_sprite_2d.play(&"idle")
+	elif input_component.direction == Vector2.LEFT:
+		animated_sprite_2d.flip_h = false
+		animated_sprite_2d.play(&"move_left")
+	elif input_component.direction == Vector2.RIGHT:
+		animated_sprite_2d.flip_h = true
+		animated_sprite_2d.play(&"move_left")
+	elif input_component.direction == Vector2.DOWN:
+		animated_sprite_2d.play(&"move_down")
+	elif input_component.direction == Vector2.UP:
+		animated_sprite_2d.play(&"move_up")
+	
+	velocity = input_component.direction * SPEED
