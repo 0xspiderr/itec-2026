b2b7b3a07fdb9fb4366b8d5743f70acb7db1acf2
Harry <harypatrascu.1@gmail.com>
2026-03-28 03:44:19 +0200

    added: hold animations for when picking up items

--- _rat/rat.gd
+++ _rat/rat.gd
@@ -6,6 +6,7 @@ class_name RatController extends CharacterBody2D
 		# set input authority to the local player
 		player_id = id
 		%InputComponent.set_multiplayer_authority(id)
+@export var player_sprite_frames: Array[SpriteFrames]
 @export var sprite_frame_index: int = 0
 
 const SPEED = 300.0
@@ -13,13 +14,22 @@ const SPEED = 300.0
 @onready var camera_2d: Camera2D = $Camera2D
 @onready var input_component: InputComponent = $InputComponent
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-@export var player_sprite_frames: Array[SpriteFrames]
+
+@export var items_cnt: int = 0
+
 const ANIM_MAP = {
 	Vector2.ZERO: &"idle",
 	Vector2.LEFT: &"walk_left",
 	Vector2.RIGHT: &"walk_left", # this will be flipped
 	Vector2.UP: &"walk_up",
 	Vector2.DOWN: &"walk_down"
+}
+const ANIM_HOLD = {
+	Vector2.ZERO: &"idle_hold",
+	Vector2.LEFT: &"walk_left_hold",
+	Vector2.RIGHT: &"walk_left_hold",
+	Vector2.UP: &"walk_up_hold",
+	Vector2.DOWN: &"walk_down_hold"
 }
 
 func _ready() -> void:
@@ -44,12 +54,14 @@ func _move_player() -> void:
 
 func _animate_player() -> void:
 	var dir = input_component.direction
-	if ANIM_MAP.has(dir):
+	if ANIM_MAP.has(dir) and items_cnt == 0:
 		animated_sprite_2d.play(ANIM_MAP[dir])
-
+	elif ANIM_HOLD.has(dir):
+		animated_sprite_2d.play(ANIM_HOLD[dir])
+	
 	if dir.x != 0:
 		animated_sprite_2d.flip_h = (dir == Vector2.RIGHT)
 
 
 func item_pickup() -> void:
-	print("hello world")
+	items_cnt += 1
